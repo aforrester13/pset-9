f9edb1bb58fd98473738ea449bcb2356cfd064c5
Aidan Forrester <Aidan@Daras-MacBook-Air.local>
2020-03-02 17:27:05 -0500

    final commit

--- js/brick-breaker.js
+++ js/brick-breaker.js
@@ -1,352 +1,187 @@
-var app = {
-
-  init : function(){
-
-    this.canvas = document.querySelector('#myGameCanvas');
-    this.context = this.canvas ? this.canvas.getContext('2d') : null;
-
-    if (!this.context)
-    {
-      console.log("Error getting application context");
-      return; //TODO: notify user
-    }
-
-    window.addEventListener('keydown', controller.keypress, true);
-    this.setupBricks();
-    this.update();
-
-    return;
-
-  },
-
-  update : function(){
-
-    app.clearContext();
-
-    app.drawBricks();
-    player.draw();
-    ball.update();
-
-    requestAnimationFrame(app.update);
-  },
-
-  clearContext : function(){
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    return;
-  },
-
-  die : function(){
-    //TODO: better death!
-    player.lives -= 1;
-
-    if (player.lives < 1)
-      this.reset();
-
-    ball.reset();
-  },
-
-  drawBricks : function(){
-
-    this.bricks.forEach(function(brick){ brick.draw(); });
-
-  },
-
-  //TODO: this will change per level
-  setupBricks : function(){
-
-    this.bricks = new Array();
-
-    var i = 0;
-    var brickTop = 50;
-    var brickBackLeft = 150;
-
-    //Setup back row:
-    for (i = 0; i < 10; i++)
-    {
-      var brick = new Brick();
-      brick.position.x = 150 + (i * brick.size.width) + i;
-      brick.position.y = brickTop;
-      brick.health = 1;
-      this.bricks.push(brick);
-    }
-
-    //Setup middle row:
-    for (i = 0; i < 8; i++)
-    {
-      var brick = new Brick();
-      brick.position.x = 201 + (i * brick.size.width) + i;
-      brick.position.y = brickTop + brick.size.height + 1;
-      brick.health = 2;
-      this.bricks.push(brick);
-    }
-
-    //Setup front row:
-    for (i = 0; i < 6; i++)
-    {
-      var brick = new Brick();
-      brick.position.x = 252 + (i * brick.size.width) + i;
-      brick.position.y = brickTop + (2 * brick.size.height) + 1;
-      brick.health = 3;
-      this.bricks.push(brick);
-    }
-
-  },
-
-  reset : function(){
-    player.reset();
-    ball.reset();
-    this.setupBricks();
-  },
-
-  bricks: [],
-
-  canvas: null,
-  context : null,
-  timeout: 33
-
-};
-
-var controller = {
-
-  keypress : function(event){
-
-    switch (event.keyCode) {
-      case 37:
-        player.moveLeft();
-        break;
-      case 39:
-        player.moveRight();
-        break;
-    }
-
-  },
-
-};
-
-var player = {
-
-  // Defines initial position
-  position: {
-    x: 375,
-    y: 480
-  },
-
-  score: 0,
-
-  lives: 3,
-
-  physics: {
-    speed: 10
-  },
-
-  size: {
-    height: 10,
-    width: 50
-  },
-
-  draw : function(){
-    app.context.fillStyle = "rgb(200, 0, 0)";
-    app.context.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
-
-    app.context.textAlign = "center";
-    app.context.fillStyle = "rgba(0, 0, 0, .2)";
-
-    app.context.font = "18px sans-serif";
-    app.context.fillText("Lives", 40, 20);
-    app.context.fillText("Score", 40, 120);
-
-    app.context.font = "48px sans-serif";
-    app.context.fillText(this.lives, 40, 75);
-    app.context.fillText(this.score, 40, 175);
-  },
-
-  moveLeft: function(){
-    if (this.position.x > 0)
-      this.position.x -= this.physics.speed;
-  },
-
-  moveRight: function(){
-    if (this.position.x < (app.canvas.width - this.size.width))
-      this.position.x += this.physics.speed;
-  },
-
-  reset: function(){
-    this.lives = 3;
-    this.score = 0;
-    this.position.x = 100;
-  }
-
-};
-
-var ball = {
-
-  position: {
-    x: 50,
-    y: 50
-  },
-
-  size: {
-    height: 10,
-    width: 10
-  },
-
-  physics: {
-    speed: 5
-  },
-
-  direction: {
-    x: 1, //Moving right
-    y: 1 //Moving down
-  },
-
-  draw : function(){
-    app.context.fillStyle = "rgb(200, 0, 0)";
-    app.context.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
-  },
-
-  reset : function(){
-    this.position.x = 50;
-    this.position.y = 50;
-    this.direction.x = 1;
-    this.direction.y = 1;
-  },
-
-  update : function(){
-
-    if (this.position.x <= 0) //Left Bounds
-      this.direction.x = 1;
-    if (this.position.x >= app.canvas.width) //Right Bounds
-      this.direction.x = -1;
-    if (this.position.y <= 0) //Top Bounds
-      this.direction.y = 1;
-    if (this.position.y >= app.canvas.height) //Bottom Bounds
-      app.die(); //TODO: die
-
-    this.checkCollisionWithPlayer();
-    this.checkCollisionWithBricks();
-
-    this.position.x += (this.physics.speed * this.direction.x);
-    this.position.y += (this.physics.speed * this.direction.y);
-
-    this.draw();
-  },
-
-  checkCollisionWithPlayer : function(){
-
-    if (this.position.y + this.size.height < player.position.y)
-      return;
-    if (this.position.y > player.position.y + player.size.height)
-      return;
-    if (this.position.x > player.position.x + player.size.width)
-      return;
-    if (this.position.x + this.size.width < player.position.x)
-      return;
-
-    this.direction.y = -1; //Moving up now
-
-  },
-
-  checkCollisionWithBricks : function(){
-
-    var i = 0;
-    for (i = 0; i < app.bricks.length; i++)
-    {
-      var brick = app.bricks[i];
-
-      if (this.position.y + this.size.height < brick.position.y)
-        continue;
-      if (this.position.y > brick.position.y + brick.size.height)
-        continue;
-      if (this.position.x > brick.position.x + brick.size.width)
-        continue;
-      if (this.position.x + this.size.width < brick.position.x)
-        continue;
-
-      /* If the loop makes it this far, we have a collision */
-      brick.health -= 1;
-
-      player.score += 20;
-
-      if (brick.health < 1)
-        app.bricks.splice(i, 1);
-
-      //Update direction based on where we hit the brick
-
-      //Moving towards lower right
-      if (this.direction.x == 1
-          && this.direction.y == 1)
-      {
-        if (this.position.y > brick.position.y)
-          this.direction.x = -1;
-        else
-          this.direction.y = -1;
-      }
-      //Moving towards lower left
-      else if (this.direction.x == -1
-               && this.direction.y == 1)
-      {
-        if (this.position.y > brick.position.y)
-          this.direction.x = 1;
-        else
-          this.direction.y = -1;
-      }
-      //Moving towards upper right
-      else if (this.direction.x == 1
-               && this.direction.y == -1)
-      {
-        if (this.position.y > brick.position.y)
-          this.direction.x = -1;
-        else
-          this.direction.y = -1;
-      }
-      //Moving towards upper-left
-      else if (this.direction.x == -1
-               && this.direction.y == -1)
-      {
-        if (this.position.y > brick.position.y)
-          this.direction.x = 1;
-        else
-          this.direction.y = -1;
-
-      }
-    }
-  }
-
-};
-
-var Brick = function(){
-
-  this.health = 3;
-
-  this.size = {
-    height: 30,
-    width: 50
-  };
-
-  //Will be determined on setup
-  this.position = {
-    x: 0,
-    y: 0
-  };
-
-};
-
-Brick.prototype.draw = function(){
-
-  switch (this.health) {
-    case 3:
-      app.context.fillStyle = "rgb(0, 200, 0)"; //Green
-      break;
-    case 2:
-      app.context.fillStyle = "rgb(200, 200, 0)"; //Orange?
-      break;
-    case 1:
-      app.context.fillStyle = "rgb(200, 0, 0)"; //Red
-      break;
-  }
-
-  if (this.health > 0)
-    app.context.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
-};
-
-app.init();
+var canvas = document.getElementById("canvas_for_game");
+	var ctx = canvas.getContext("2d");
+  var paddleHeight = 10;
+	var paddleWidth = 75;
+	var paddleX = (canvas.width-paddleWidth)/2;
+	var rightPressed = false;
+	var leftPressed = false;
+	var brickRowCount = 4;
+	var brickColumnCount = 9;
+	var brickWidth = 70;
+	var brickHeight = 20;
+	var brickPadding = 15;
+	var brickOffsetTop = 30;
+	var brickOffsetLeft = 30;
+	var brickColor = "red";
+	var ballRadius = 10;
+	var ballColor = "blue"
+	var speed = 10;
+	var x = canvas.width/2;
+	var y = canvas.height-30;
+	var lives = 3
+	var dx = 2;
+	var dy = -2;
+	var px = 7;
+
+
+	var bricks = [];
+	for(c = 0; c < brickColumnCount; c++) {
+		bricks[c] = [];
+		for(r=0; r<brickRowCount; r++) {
+			bricks[c][r] = { x: 0, y: 0, status: 1 };
+		}
+	}
+
+	var score = 0;
+
+
+	document.addEventListener("keydown", keyDownHandler, false);
+	document.addEventListener("keyup", keyUpHandler, false);
+
+	function drawBall() {
+		ctx.beginPath();
+		ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+		ctx.fillStyle = ballColor;
+		ctx.fill();
+		ctx.closePath();
+	}
+
+	function drawPaddle() {
+		ctx.beginPath();
+		ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+		ctx.fillStyle = ballColor;
+		ctx.fill();
+		ctx.closePath();
+	}
+
+	function drawBricks() {
+		for(c = 0; c < brickColumnCount; c++) {
+			for(r = 0; r < brickRowCount; r++) {
+				if(bricks[c][r].status == 1) {
+					var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+					var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+					bricks[c][r].x = brickX;
+					bricks[c][r].y = brickY;
+
+					ctx.beginPath();
+					ctx.rect(brickX, brickY, brickWidth, brickHeight);
+					ctx.fillStyle = brickColor;
+					ctx.fill();
+					ctx.closePath();
+				}
+			}
+		}
+	}
+
+	function keyDownHandler(e) {
+		if(e.keyCode == 39) {
+			rightPressed = true;
+		}
+		else if(e.keyCode == 37) {
+			leftPressed = true;
+		}
+	}
+
+	function keyUpHandler(e) {
+		if(e.keyCode == 39) {
+			rightPressed = false;
+		}
+		else if(e.keyCode == 37) {
+			leftPressed = false;
+		}
+	}
+
+	function collisionDetection() {
+		for(c=0; c<brickColumnCount; c++) {
+			for(r=0; r<brickRowCount; r++) {
+				var b = bricks[c][r];
+				if(b.status == 1) {
+					if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+						dy = -dy;
+						b.status = 0;
+						score++;
+
+						if(score == brickRowCount*brickColumnCount) {
+							alert("Great Job! You win!");
+							document.location.reload();
+						}
+					}
+				}
+			}
+		}
+	}
+
+	function drawScore() {
+		ctx.font = "22px Georgia";
+		ctx.fillStyle = "Black";
+		ctx.fillText("Bricks Broken: "+score, 338, 20);
+	}
+
+	function drawLives() {
+  ctx.font = "22px Lato";
+  ctx.fillStyle = "black";
+  ctx.fillText("Lives: "+lives, 638, 23);
+}
+
+
+	function draw() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+		drawBricks();
+		drawBall();
+		drawPaddle();
+		collisionDetection();
+		drawScore();
+		drawLives();
+
+		x += dx;
+		y += dy;
+
+		if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+			dx = -dx;
+			ballColor = getRandomColor();
+		}
+		if(y + dy < ballRadius) {
+			dy = -dy;
+		} else if(y + dy > canvas.height-ballRadius) {
+			if(x > paddleX && x < paddleX + paddleWidth) {
+				dy = -dy;
+			}
+			else {
+				{
+				    lives--;
+				 if(!lives) {
+				        alert("Game Over. Try Again!");
+				        document.location.reload();
+				      } else {
+				        x = canvas.width/2;
+				        y = canvas.height-30;
+				        dx = 3;
+				        dy = -3;
+				        paddleX = (canvas.width-paddleWidth)/2;
+				      }
+			}
+		}
+	}
+
+		if(rightPressed && paddleX < canvas.width-paddleWidth) {
+			paddleX += px;
+		}
+		else if(leftPressed && paddleX > 0) {
+			paddleX -= px;
+		}
+
+		if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+			dx = -dx;
+		}
+
+		if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+			dy = -dy;
+		}
+	}
+
+	setInterval(draw, speed);
+
+  document.getElementById("homeButton").onclick = function () {
+          location.href = "index.html";
+      };
